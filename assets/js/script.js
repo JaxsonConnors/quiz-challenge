228640f9fe69835153672bee73b369b04397e498
Jaxson Connors <connorsjaxson@gmail.com>
2021-08-13 22:29:57 -0600

    started questions

---
+++ assets/js/script.js
@@ -0,0 +1,255 @@
+var body = document.body;
+// h1 element
+var h1El = document.createElement('h1');
+h1El.textContent = 'Welcome to my page';
+h1El.setAttribute('style', 'margin:auto; width:50%; text-align:center;');
+body.appendChild(h1El);
+
+// questions
+
+//----1
+var q1 = document.createElement('h2');
+q1.textContent =
+  'This is a question';
+q1.setAttribute('style', 'margin:auto; width:100%; text-align:center;');
+body.appendChild(q1);
+
+var favoriteEl = document.createElement('div');
+var listEl = document.createElement('ol');
+var li1 = document.createElement('li');
+var li2 = document.createElement('li');
+var li3 = document.createElement('li');
+var li4 = document.createElement('li');
+
+favoriteEl.textContent = 'Choose One:';
+li1.textContent = 'Answer A';
+li2.textContent = 'Answer B';
+li3.textContent = 'Answer C';
+li4.textContent = 'Answer D';
+
+favoriteEl.setAttribute('style', 'font-size:20px;');
+listEl.setAttribute('style', 'background: #888888; padding:20px;');
+
+body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+
+//----2
+var q2 = document.createElement('h2');
+q2.textContent =
+  'This is a question';
+q2.setAttribute('style', 'margin:auto; width:100%; text-align:center;');
+body.appendChild(q2);
+
+var favoriteEl = document.createElement('div');
+var listEl = document.createElement('ol');
+var li1 = document.createElement('li');
+var li2 = document.createElement('li');
+var li3 = document.createElement('li');
+var li4 = document.createElement('li');
+
+favoriteEl.textContent = 'Choose One:';
+li1.textContent = 'Answer A';
+li2.textContent = 'Answer B';
+li3.textContent = 'Answer C';
+li4.textContent = 'Answer D';
+
+favoriteEl.setAttribute('style', 'font-size:20px;');
+listEl.setAttribute('style', 'background: #888888; padding:20px;');
+
+body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+
+//----3
+var q3 = document.createElement('h2');
+q3.textContent =
+  'This is a question';
+q3.setAttribute('style', 'margin:auto; width:100%; text-align:center;');
+body.appendChild(q3);
+
+var favoriteEl = document.createElement('div');
+var listEl = document.createElement('ol');
+var li1 = document.createElement('li');
+var li2 = document.createElement('li');
+var li3 = document.createElement('li');
+var li4 = document.createElement('li');
+
+favoriteEl.textContent = 'Choose One:';
+li1.textContent = 'Answer A';
+li2.textContent = 'Answer B';
+li3.textContent = 'Answer C';
+li4.textContent = 'Answer D';
+
+favoriteEl.setAttribute('style', 'font-size:20px;');
+listEl.setAttribute('style', 'background: #888888; padding:20px;');
+
+body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+
+//----4
+var q4 = document.createElement('h2');
+q4.textContent =
+  'This is a question';
+q4.setAttribute('style', 'margin:auto; width:100%; text-align:center;');
+body.appendChild(q4);
+
+var favoriteEl = document.createElement('div');
+var listEl = document.createElement('ol');
+var li1 = document.createElement('li');
+var li2 = document.createElement('li');
+var li3 = document.createElement('li');
+var li4 = document.createElement('li');
+
+favoriteEl.textContent = 'Choose One:';
+li1.textContent = 'Answer A';
+li2.textContent = 'Answer B';
+li3.textContent = 'Answer C';
+li4.textContent = 'Answer D';
+
+favoriteEl.setAttribute('style', 'font-size:20px;');
+listEl.setAttribute('style', 'background: #888888; padding:20px;');
+
+body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+
+//----5
+var q5 = document.createElement('h2');
+q5.textContent =
+  'This is a question';
+q5.setAttribute('style', 'margin:auto; width:100%; text-align:center;');
+body.appendChild(q5);
+
+var favoriteEl = document.createElement('div');
+var listEl = document.createElement('ol');
+var li1 = document.createElement('li');
+var li2 = document.createElement('li');
+var li3 = document.createElement('li');
+var li4 = document.createElement('li');
+
+favoriteEl.textContent = 'Choose One:';
+li1.textContent = 'Answer A';
+li2.textContent = 'Answer B';
+li3.textContent = 'Answer C';
+li4.textContent = 'Answer D';
+
+favoriteEl.setAttribute('style', 'font-size:20px;');
+listEl.setAttribute('style', 'background: #888888; padding:20px;');
+
+body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+
+//----6
+var q6 = document.createElement('h2');
+q6.textContent =
+  'This is a question';
+q6.setAttribute('style', 'margin:auto; width:100%; text-align:center;');
+body.appendChild(q6);
+
+var favoriteEl = document.createElement('div');
+var listEl = document.createElement('ol');
+var li1 = document.createElement('li');
+var li2 = document.createElement('li');
+var li3 = document.createElement('li');
+var li4 = document.createElement('li');
+
+favoriteEl.textContent = 'Choose One:';
+li1.textContent = 'Answer A';
+li2.textContent = 'Answer B';
+li3.textContent = 'Answer C';
+li4.textContent = 'Answer D';
+
+favoriteEl.setAttribute('style', 'font-size:20px;');
+listEl.setAttribute('style', 'background: #888888; padding:20px;');
+
+body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+
+//----7
+var q7 = document.createElement('h2');
+q7.textContent =
+  'This is a question';
+q7.setAttribute('style', 'margin:auto; width:100%; text-align:center;');
+body.appendChild(q7);
+
+var favoriteEl = document.createElement('div');
+var listEl = document.createElement('ol');
+var li1 = document.createElement('li');
+var li2 = document.createElement('li');
+var li3 = document.createElement('li');
+var li4 = document.createElement('li');
+
+favoriteEl.textContent = 'Choose One:';
+li1.textContent = 'Answer A';
+li2.textContent = 'Answer B';
+li3.textContent = 'Answer C';
+li4.textContent = 'Answer D';
+
+favoriteEl.setAttribute('style', 'font-size:20px;');
+listEl.setAttribute('style', 'background: #888888; padding:20px;');
+
+body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+
+//----8
+var q8 = document.createElement('h2');
+q8.textContent =
+  'This is a question';
+q8.setAttribute('style', 'margin:auto; width:100%; text-align:center;');
+body.appendChild(q8);
+
+var favoriteEl = document.createElement('div');
+var listEl = document.createElement('ol');
+var li1 = document.createElement('li');
+var li2 = document.createElement('li');
+var li3 = document.createElement('li');
+var li4 = document.createElement('li');
+
+favoriteEl.textContent = 'Choose One:';
+li1.textContent = 'Answer A';
+li2.textContent = 'Answer B';
+li3.textContent = 'Answer C';
+li4.textContent = 'Answer D';
+
+favoriteEl.setAttribute('style', 'font-size:20px;');
+listEl.setAttribute('style', 'background: #888888; padding:20px;');
+
+body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
